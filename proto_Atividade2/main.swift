20abbcee7310b48debd67af9952b886dc9ee40a1
bretondato <brenno.tondato@gmail.com>
2019-06-18 19:03:09 -0300

    Initial Commit

---
+++ proto_Atividade2/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  proto_Atividade2
+//
+//  Created by Brenno Tondato de Faria on 18/06/19.
+//  Copyright © 2019 Brenno Tondato de Faria. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
